78ba5194fd546f45cb7f60d40852f527aed87f67
Jhonn-Lopez <jho.lopezm@duocuc.cl>
2023-11-19 02:42:20 -0300

    Solucion carga de imagenes

--- Content/screens/CursoNoIniScreen.js
+++ Content/screens/CursoNoIniScreen.js
@@ -3,7 +3,7 @@ import { View, Text, FlatList, TouchableOpacity, StyleSheet, Image, Alert } from
 import { useNavigation } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
 import axios from 'axios';
-import * as SecureStore from 'expo-secure-store'; // Asegúrate de importar SecureStore
+import * as SecureStore from 'expo-secure-store'; // Importa SecureStore
 
 const CursoNoIniScreen = () => {
     const [cursos, setCursos] = useState([]);
@@ -11,74 +11,108 @@ const CursoNoIniScreen = () => {
 
     useEffect(() => {
         const fetchCursosNoIniciados = async () => {
-            try {
-                const token = await SecureStore.getItemAsync('userToken');
-                if (token) {
+            const token = await SecureStore.getItemAsync('userToken');
+            if (token) {
+                try {
                     const response = await axios.get('http://localhost:8000/api/progreso_curso_no_iniciado/', {
                         headers: {
                             'Authorization': `Token ${token}`
                         }
                     });
                     setCursos(response.data);
-                } else {
-                    Alert.alert('Error', 'No se encontró el token de autenticación.');
+                } catch (error) {
+                    console.error('Error al obtener cursos no iniciados:', error);
+                    Alert.alert('Error', 'No se pudo obtener los cursos no iniciados.');
                 }
-            } catch (error) {
-                console.error('Error al obtener cursos no iniciados:', error);
-                Alert.alert('Error', 'No se pudo obtener los cursos no iniciados.');
             }
         };
 
         fetchCursosNoIniciados();
     }, []);
 
-    const iniciarCurso = (cursoId) => {
-        // Reemplazar 'URL_API_BACKEND' con la URL real de tu backend
-        axios.post(`http://localhost:8000/api/iniciar_curso/${cursoId}`, {}, {
-            headers: {
-                'Authorization': `Token ${token}` // Asegúrate de obtener el token como se hace en fetchCursosNoIniciados
+    const iniciarCurso = async (cursoId) => {
+        const token = await SecureStore.getItemAsync('userToken');
+        if (token) {
+            try {
+                await axios.post(`http://localhost:8000/api/iniciar_curso/${cursoId}`, {}, {
+                    headers: {
+                        'Authorization': `Token ${token}`
+                    }
+                });
+                navigation.navigate('CursoModulos', { cursoId });
+            } catch (error) {
+                console.error('Error al iniciar curso:', error);
+                Alert.alert('Error', 'No se pudo iniciar el curso.');
             }
-        })
-        .then(() => {
-            navigation.navigate('CursoModulos', { cursoId });
-        })
-        .catch(error => {
-            console.error('Error al iniciar curso:', error);
-            Alert.alert('Error', 'No se pudo iniciar el curso.');
-        });
+        }
     };
 
-    // Aquí puedes manejar la lógica del header si es necesario
-    // ...
+    useEffect(() => {
+        navigation.setOptions({
+            headerShown: true,
+            headerLeft: () => (
+                <TouchableOpacity
+                    style={styles.headerButton}
+                    onPress={() => navigation.goBack()}>
+                    <Ionicons
+                        name="arrow-back"
+                        size={24}
+                        color="black"
+                    />
+                </TouchableOpacity>
+            ),
+            headerRight: () => (
+                <TouchableOpacity
+                    style={styles.headerButton}
+                    onPress={() => navigation.toggleDrawer()}>
+                    <Ionicons
+                        name="md-menu"
+                        size={24}
+                        color="black"
+                    />
+                </TouchableOpacity>
+            ),
+            title: 'Cursos No Iniciados',
+        });
+    }, [navigation]);
 
     return (
         <View style={styles.container}>
             <FlatList
                 data={cursos}
-                renderItem={({ item }) => (
-                    <View style={styles.cursoItem}>
-                        <Text style={styles.cursoTitle}>{item.curso.nombre}</Text>
-                        <Text>{item.curso.descripcion}</Text>
-                        <Image source={{ uri: `http://localhost:8000${item.curso.imagen}` }} style={styles.cursoImage} />
-
-                        <TouchableOpacity
-                            style={styles.iniciarButton}
-                            onPress={() => iniciarCurso(item.id)}>
-                            <Text style={styles.iniciarButtonText}>Iniciar Curso</Text>
-                        </TouchableOpacity>
-                    </View>
-                )}
+                renderItem={({ item }) => {
+                    // Obtén la parte del path de la imagen sin la base URL
+                    const imageRelativePath = item.curso.imagen.replace('http://localhost:8000/', '');
+    
+                    // Construye la URL final incluyendo '/api/' en medio
+                    const imageUrl = `http://localhost:8000/api/${imageRelativePath}`;
+    
+                    // Imprime la URL en la consola para verificar que es correcta
+                    console.log('Cargando imagen:', imageUrl);
+    
+                    return (
+                        <View style={styles.cursoItem}>
+                            <Text style={styles.cursoTitle}>{item.curso.nombre}</Text>
+                            <Text style={styles.cursoDescription}>{item.curso.descripcion}</Text>
+                            <View style={styles.cursoImageContainer}>
+                            <Image source={{ uri: imageUrl }} style={styles.cursoImage} />
+                            </View>                        
+                            <TouchableOpacity
+                                style={styles.iniciarButton}
+                                onPress={() => iniciarCurso(item.id)}>
+                                <Text style={styles.iniciarButtonText}>Iniciar Curso</Text>
+                            </TouchableOpacity>
+                        </View>
+                    );
+                }}
                 keyExtractor={item => item.id.toString()}
             />
         </View>
     );
+    
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 10,
-    },
     headerButton: {
         paddingHorizontal: 10,
     },
@@ -86,28 +120,36 @@ const styles = StyleSheet.create({
         padding: 10,
         borderBottomWidth: 1,
         borderBottomColor: '#ccc',
-        marginBottom: 10,
     },
     cursoTitle: {
-        fontSize: 18,
+        fontSize: 16,
         fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    cursoDescription: {
+        fontSize: 12,
+        textAlign: 'justify'
+    },
+    cursoImageContainer: {
+        width: '100%', // Asume el ancho completo del contenedor
+        height: 180, // Altura fija para el contenedor de la imagen
+                justifyContent: 'center', // Centra la imagen verticalmente
+        alignItems: 'center', // Centra la imagen horizontalmente
+        marginVertical: 10, // Espaciado vertical para separar el contenedor de imagen de otros elementos
     },
     cursoImage: {
-        width: '100%',     // Utiliza el ancho completo del contenedor
-        height: 200,       // Altura fija para la imagen
-        resizeMode: 'contain', // Asegura que la imagen se ajuste dentro del espacio sin recortarse
+        width: '100%', // Ancho fijo para todas las imágenes
+        height: '100%', // Altura fija para todas las imágenes
+        resizeMode: 'stretch', // La imagen cubrirá el espacio asignado, posiblemente recortándose
         marginVertical: 10, // Espaciado vertical para separar la imagen de otros elementos
-        backgroundColor: 'lightgray', // Fondo para identificar si el contenedor de la imagen está presente
     },
+    
+    
     iniciarButton: {
         backgroundColor: 'blue',
         padding: 10,
         alignItems: 'center',
         borderRadius: 5,
-    },
-    iniciarButtonText: {
-        color: 'white',
-        fontWeight: 'bold',
     },
     // ... más estilos si es necesario ...
 });
