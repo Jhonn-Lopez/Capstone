5d77559a740e1cf4fbec62dd04e69e25413b310e
Jhonn-Lopez <jho.lopezm@duocuc.cl>
2023-11-28 00:30:21 -0300

    Avance curso modulo

--- Content/screens/CursoModulosScreen.js
+++ Content/screens/CursoModulosScreen.js
@@ -1,109 +1,118 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, SectionList, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { ScrollView, View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import Accordion from 'react-native-collapsible/Accordion';
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
-import Collapsible from 'react-native-collapsible';
 
-const CursoModulosScreen = ({ route }) => {
+const CursoModulosScreen = ({ route, navigation }) => {
     const { cursoId } = route.params;
-    const [modulos, setModulos] = useState([]);
-    const [activeIndex, setActiveIndex] = useState(0);
+    const [activeSections, setActiveSections] = useState([]);
+    const [cursoData, setCursoData] = useState(null); // Estado para almacenar la información del curso actual
+    const [modulos, setModulos] = useState([]); // Estado para almacenar los módulos del curso
 
     useEffect(() => {
-        const fetchModulos = async () => {
-            const token = await SecureStore.getItemAsync('userToken');
-            console.log("Token obtenido:", token); // Log para verificar el token recuperado
-
-            if (token) {
-                try {
-                    const response = await axios.get(`http://localhost:8000/api/cursos/${cursoId}/modulos`, {
-                        headers: {
-                            'Authorization': `Token ${token}`
-                        }
-                    });
-                    console.log("Datos recibidos:", response.data); // Log para verificar los datos recibidos
-                    setModulos(response.data.map((modulo, index) => ({
-                        ...modulo,
-                        isCollapsed: index !== 0,
-                    })));
-                } catch (error) {
-                    console.error('Error al obtener módulos:', error);
-                    Alert.alert('Error', 'No se pudieron obtener los módulos del curso.');
-                }
-            } else {
-                console.log("Token no encontrado o inválido."); // Log en caso de que el token no exista o sea inválido
-            }
-        };
-
-        fetchModulos();
+        fetchCursoData(cursoId);
     }, [cursoId]);
 
-    const toggleCollapse = index => {
-        setModulos(modulos.map((modulo, i) => ({
-            ...modulo,
-            isCollapsed: i !== index ? true : !modulo.isCollapsed // Cambia la visibilidad del módulo seleccionado
-        })));
-        setActiveIndex(index); // Actualiza el índice activo
+    const fetchCursoData = async (id) => {
+        const token = await SecureStore.getItemAsync('userToken');
+        try {
+            const responseCurso = await axios.get(`http://localhost:8000/api/cursos/${id}/`, {
+                headers: { 'Authorization': `Token ${token}` },
+            });
+            setCursoData(responseCurso.data);
+
+            // Suponiendo que la API devuelve los módulos anidados en los datos del curso
+            setModulos(responseCurso.data.modulos);
+        } catch (error) {
+            console.error('Error al obtener los datos del curso:', error);
+        }
     };
 
+    const renderHeader = (modulo, _, isActive) => {
+        return (
+            <View style={styles.header}>
+                <Text style={styles.headerText}>{modulo.nombre}</Text>
+            </View>
+        );
+    };
 
-    
+    const renderContent = (modulo) => {
+        // Aquí puedes decidir mostrar el contenido basado en si el módulo está activo o no
+        return (
+            <View style={styles.content}>
+                <Text style={styles.contentText}>{modulo.descripcion}</Text>
+                {/* Aquí también puedes mostrar contenido, preguntas y respuestas si están disponibles */}
+            </View>
+        );
+    };
+
     return (
-        <View style={styles.container}>
-            <SectionList
-                sections={[ // Puedes tener varias secciones si tu curso está dividido en categorías
-                    { title: 'Módulos del Curso', data: modulos },
-                ]}
-                keyExtractor={(item, index) => item + index}
-                renderItem={({ item, index }) => (
-                    <View>
-                        <TouchableOpacity onPress={() => toggleCollapse(index)}>
-                            <View style={styles.header}>
-                                <Text style={styles.headerText}>{item.nombre}</Text>
-                            </View>
-                        </TouchableOpacity>
-                        <Collapsible collapsed={item.isCollapsed}>
-                            <View style={styles.content}>
-                                <Text>{item.descripcion}</Text>
-                                {/* Aquí iría el resto del contenido del módulo */}
-                            </View>
-                        </Collapsible>
-                    </View>
-                )}
-                renderSectionHeader={({ section: { title } }) => (
-                    <Text style={styles.sectionHeader}>{title}</Text>
-                )}
+        <ScrollView style={styles.container}>
+            <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
+                <Text style={styles.backButtonText}>{"<"}</Text> {/* Icono de regresar */}
+            </TouchableOpacity>
+
+            {cursoData && cursoData.imagen && (
+                <Image source={{ uri: cursoData.imagen }} style={styles.moduleImage} blurRadius={1} />
+            )}
+
+            <Accordion
+                sections={modulos}
+                activeSections={activeSections}
+                renderHeader={renderHeader}
+                renderContent={renderContent}
+                onChange={setActiveSections}
+                underlayColor="transparent"
+                sectionContainerStyle={styles.sectionContainer}
             />
-        </View>
+
+            {/* Agregar más componentes si es necesario */}
+        </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        marginTop: 20,
+        backgroundColor: '#f0f0f0',
     },
-    sectionHeader: {
-        fontWeight: '800',
-        fontSize: 18,
-        color: '#f4f4f4',
-        backgroundColor: '#6b52ae',
-        padding: 10,
+    backButton: {
+        marginTop: 50, // Ajusta de acuerdo al safe area y al estilo de tu aplicación
+        marginLeft: 10,
+    },
+    backButtonText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    moduleImage: {
+        width: '100%',
+        height: 200,
+        resizeMode: 'cover',
     },
     header: {
-        backgroundColor: '#f8f8f8',
+        backgroundColor: '#fff',
         padding: 10,
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        borderColor: '#ddd',
     },
     headerText: {
-        textAlign: 'center',
         fontSize: 16,
-        fontWeight: '500',
+        fontWeight: 'bold',
     },
     content: {
         padding: 20,
         backgroundColor: '#fff',
     },
-    // ... más estilos si es necesario ...
+    contentText: {
+        fontSize: 14,
+    },
+    sectionContainer: {
+        backgroundColor: '#fff',
+        marginBottom: 10,
+    },
+    // ... más estilos según sea necesario ...
 });
 
 export default CursoModulosScreen;
