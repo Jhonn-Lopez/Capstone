3ada487929cd3c340900e301a9c1189e88ec5382
Jhonn-Lopez <jho.lopezm@duocuc.cl>
2023-11-29 23:15:29 -0300

    Validacion verificacion de cursos Home

--- Content/screens/HomeScreen.js
+++ Content/screens/HomeScreen.js
@@ -20,7 +20,7 @@ const HomeScreen = () => {
         if (response.data && response.data.length > 0) {
           navigation.navigate('CursoCompletado');
         } else {
-          Alert.alert("No hay cursos completados", "Revise si tiene cursos activos sin terminar!");
+          Alert.alert("No hay cursos completados", "Puedes revisar si tiene cursos activos pendientes!");
         }
       } catch (error) {
         console.error('Error al verificar cursos completados:', error);
@@ -29,16 +29,58 @@ const HomeScreen = () => {
     }
   }, [navigation]);
 
+  const verificarCursosActivos = useCallback(async () => {
+    const token = await SecureStore.getItemAsync('userToken');
+    if (token) {
+      try {
+        const response = await axios.get('http://localhost:8000/api/progreso_curso_activo/', {
+          headers: {
+            'Authorization': `Token ${token}`
+          }
+        });
+        if (response.data && response.data.length > 0) {
+          navigation.navigate('CursoActivo');
+        } else {
+          Alert.alert("No hay cursos activos", "Puedes revisar tus cursos sin iniciar o completados!");
+        }
+      } catch (error) {
+        console.error('Error al verificar cursos activos:', error);
+        Alert.alert('Error', 'No se pudo verificar los cursos activos.');
+      }
+    }
+  }, [navigation]);
+
+  const verificarCursosSinIniciar = useCallback(async () => {
+    const token = await SecureStore.getItemAsync('userToken');
+    if (token) {
+      try {
+        const response = await axios.get('http://localhost:8000/api/progreso_curso_no_iniciado/', {
+          headers: {
+            'Authorization': `Token ${token}`
+          }
+        });
+        if (response.data && response.data.length > 0) {
+          navigation.navigate('CursoNoIniciado');
+        } else {
+          Alert.alert("No hay nuevos cursos disponibles", "Haz iniciado todos los cursos disponibles!");
+        }
+      } catch (error) {
+        console.error('Error al verificar cursos sin iniciar:', error);
+        Alert.alert('Error', 'No se pudo verificar los cursos sin iniciar.');
+      }
+    }
+  }, [navigation]);
+
   return (
     <View className="flex items-center mx-4 space-y-4 pt-5">
-      <TouchableOpacity onPress={() => navigation.navigate('CursoActivo')}
+      <TouchableOpacity onPress={verificarCursosActivos}
         className="w-full bg-yellow-500 p-3 rounded-2xl mb-3">
         <Text className="text-xl font-bold text-blue-950 text-center">
           Cursos Activos
         </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => navigation.navigate('CursoNoIniciado')}
+      <TouchableOpacity onPress={verificarCursosSinIniciar}
         className="w-full bg-yellow-500 p-3 rounded-2xl mb-3">
         <Text className="text-xl font-bold text-blue-950 text-center">
           Cursos Sin Iniciar
